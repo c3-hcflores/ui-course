d441ac538f6f352b9f5c4bfdd3f38ded1368a9bc
Héctor Flores <hctor.flores@c3.ai>
2022-11-30 12:11:20 -0600

    with modal not working

--- C3UI_Starter_Repo_Advanced/c3training/lightbulbEnergy/src/ui/UiSdlApplicationStateLBE.ts
+++ C3UI_Starter_Repo_Advanced/c3training/lightbulbEnergy/src/ui/UiSdlApplicationStateLBE.ts
@@ -1,5 +1,7 @@
 import { setConfigInApplicationState } from '@c3/ui/UiSdlApplicationState';
 
+
+
 export function rowRecordStoreReducer(state, action) {
   const appStateId = action.payload.componentId;
   return setConfigInApplicationState(appStateId, state, ['rowRecord'], action.payload.obj.id);
@@ -15,6 +17,8 @@ export function storeRowRecordAction(id, obj) {
     }
 }
 
+
+
 export function apartmentIdStoreReducer(state, action) {
   const appStateId = action.payload.componentId;
   return setConfigInApplicationState(appStateId, state, ['applicationIdRecord'], action.payload.obj.id);
@@ -23,6 +27,23 @@ export function apartmentIdStoreReducer(state, action) {
 export function apartmentIdRecordAction(id, obj) {
     return {
       type: id + '.APPLICATION_ID_RECORD_STORE',
+      payload: {
+        componentId: id,
+        obj: obj
+      }
+    }
+}
+
+
+
+export function buildingIdStoreReducer(state, action) {
+  const appStateId = action.payload.componentId;
+  return setConfigInApplicationState(appStateId, state, ['buildingIdRecord'], action.payload.obj.id);
+}
+
+export function buildingIdRecordAction(id, obj) {
+    return {
+      type: id + '.BUILDING_ID_RECORD_STORE',
       payload: {
         componentId: id,
         obj: obj
